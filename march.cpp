af2febd4ea2d43ecb27c15a1328b21a0a68c845c
Kumar Gaurav <kumgaurav5620@gmail.com>
2023-07-23 10:48:28 +0530

    adding left over

--- march.cpp
+++ march.cpp
@@ -1,8 +1,10 @@
 #include "gauravlib.h"
 
 	// predictor step for interior
-void Predictor(vector<CV>& w,  vector<CV>& wl, vector<CV>& wr, double& om, double alpha, double dt)
+void Predictor(vector<CV>& w,  vector<CV>& wl, vector<CV>& wr, double dt)
 {  	
+	BC(w);
+	Edge(w,wl,wr);
 	vector<CV> wtemp(w);
 	
 	for (int j = 2; j < res-2; j++)
@@ -18,7 +20,7 @@ void Predictor(vector<CV>& w,  vector<CV>& wl, vector<CV>& wr, double& om, doubl
 }
 
 // corrector step for interior
-void Corrector(vector<CV>& w,  vector<CV>& wl, vector<CV>& wr, vector<CV>& w_init, double& om, double alpha, double om_init, double dt)
+void Corrector(vector<CV>& w,  vector<CV>& wl, vector<CV>& wr, vector<CV>& w_init, double dt)
 {	
 	BC(w);
 	Edge(w,wl,wr);
@@ -26,28 +28,21 @@ void Corrector(vector<CV>& w,  vector<CV>& wl, vector<CV>& wr, vector<CV>& w_ini
 
 	for (int j=2; j < res-2; j++)
 	{
-	FS hr= Hx(wr[j],wl[j+1]);
-	FS hl= Hx(wr[j-1],wl[j]);
-	FS source=Source( wtemp[j], wr[j-1], wl[j], wr[j], wl[j+1]);	
-	w[j].Modify ( w_init[j].p * weight + (1 - weight) * (wtemp[j].p - ((hr.p - hl.p) / dx - source.p) * dt)
-	, w_init[j].q * weight + (1 - weight) * (wtemp[j].q - ((hr.q - hl.q) / dx - source.q) * dt)
-	,  w_init[j].r * weight + (1 - weight) * (wtemp[j].r - ((hr.r - hl.r) / dx - source.r) * dt));
-	
+		FS hr= Hx(wr[j],wl[j+1]);
+		FS hl= Hx(wr[j-1],wl[j]);
+		FS source=Source( wtemp[j], wr[j-1], wl[j], wr[j], wl[j+1]);	
+		w[j].Modify ( w_init[j].p * weight + (1 - weight) * (wtemp[j].p - ((hr.p - hl.p) / dx - source.p) * dt)
+		, w_init[j].q * weight + (1 - weight) * (wtemp[j].q - ((hr.q - hl.q) / dx - source.q) * dt)
+		,  w_init[j].r * weight + (1 - weight) * (wtemp[j].r - ((hr.r - hl.r) / dx - source.r) * dt));
 	}	
 }
 
-double March (vector<CV>& w, double final_t)
+void March (vector<CV>& w, double final_t)
 {	
 	double dt = dx / 4;
-	double alpha=0,alpha_sys=0, om=0;
 	static int timesteps=0;
 	double sum1=0;
 	vector<CV> wl(w),wr(w);
-	
-	double inertia=0,integral1=0,integral2=0;
-	
-	AMB amb(integral1,integral2);
-	
 	
 	string file=string("output/files_")+to_string(delta)+string("_")+to_string(omega_initial)+string("/data");
 	if(!filesystem::exists(file))
@@ -60,69 +55,30 @@ double March (vector<CV>& w, double final_t)
 		{
 			string file1=file+string("/field_")+to_string(int((slides)*1000+timesteps/dump))+string(".csv");
 			Write(w, file1);
-		   file1=string("output/files_")+to_string(delta)+string("_")+to_string(omega_initial)+string("/omega.txt");
-			Write(omega+epsilon*om,t,file1);
 		}
-		if (t>check_t)
-		{	
-			double max_u=1e-5;
-			for(int j=0;j<res;j++)
-			{
-				if (w[j].u>max_u)
-					max_u=w[j].u;	
-			}
-			if (max_u<5e-4)
-			{
-				par["time"]=to_string(past_time+t);
-				return omega+epsilon*om;
-			}
-			check_t++;
-		}
+		
 		vector<CV> w_init(w);
-	double om_init=om;
 		
-		BC(w);
-		Edge(w,wl,wr);
-		if (t<1e-12)
-		{
-			Integrals(integral1,integral2,w,wl,wr);
-			amb=AMB(integral1,integral2);
-			
-		}
 		
-		Alpha_fric(w,wl,wr,om,alpha);
-		Predictor(w,wl,wr,om,alpha,dt);
-
-		Integrals(integral1,integral2,w,wl,wr);
-		Ang_mom(amb,integral1,integral2,true);
-		inertia=AMB_corrector(w,wl,wr,true);
-		alpha_sys=Alpha_sys( om , dt, amb, inertia );
-	
-
-		double alpha_init=alpha;
-		Alpha_fric(w,wl,wr,om,alpha);	
-		Corrector(w, wl, wr, w_init, om, alpha, om_init, dt);
-
-		Integrals(integral1,integral2,w,wl,wr);
-		Ang_mom(amb,integral1,integral2,true);
-		inertia=AMB_corrector(w,wl,wr,true);
-		alpha_sys=Alpha_sys( om , dt, amb, inertia );
-		cout<<"angular acc "<<alpha_sys-((1-weight)*alpha-weight*(alpha_init))<<"  "<<alpha<<endl;
+		Predictor(w,wl,wr,dt);
+		Corrector(w, wl, wr, w_init, dt);
 		Shed(w);
-		
 		Time_step(wl,wr,dt,t,timesteps);
 
 		double sum=0;
 		for (int i=2;i<res-2;i++)
 			sum+=w[i].r*dx;
-
-
+		if (t>check_t)
+		{	
+			if (sum<0.1*epsilon)
+				return;
+			check_t++;
+		}
 		cout<<std::setprecision(18)<<t<<"  "<<sum<<endl;
 		sum1=sum;
 		
 	}
-	par["time"]=to_string(past_time+t);
-	return omega+epsilon*om;
+		par["time"]=to_string(past_time+t);
 }
 
 
